a94d83616de6e935a60832ec6a9d6e0976f8f5d9
Savage <savageboy74@gmail.com>
2016-10-15 01:19:36 -0400

    Fix typo

--- app/Savage/Http/Guard.php
+++ app/Savage/Http/Guard.php
@@ -6,7 +6,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
 /**
- * Gaurd extends Slim\Csrf\Gaurd to add the ability to exclude routes from CSRF protection.
+ * Guard extends Slim\Csrf\Guard to add the ability to exclude routes from CSRF protection.
  */
 
 class Guard extends CSRF
