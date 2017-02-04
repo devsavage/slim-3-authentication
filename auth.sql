db4426071e88118f8137b45deeed359ed3987ccb
Savage <savageboy74@gmail.com>
2017-02-03 20:39:09 -0500

    Update SQL file

--- auth.sql
+++ auth.sql
@@ -2,7 +2,7 @@
 -- Host:                         127.0.0.1
 -- Server version:               5.6.25 - MySQL Community Server (GPL)
 -- Server OS:                    Win32
--- HeidiSQL Version:             9.4.0.5145
+-- HeidiSQL Version:             9.4.0.5146
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -12,11 +12,11 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Dumping database structure for auth-v2
-CREATE DATABASE IF NOT EXISTS `auth-v2` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `auth-v2`;
+-- Dumping database structure for auth
+CREATE DATABASE IF NOT EXISTS `auth` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `auth`;
 
--- Dumping structure for table auth-v2.users
+-- Dumping structure for table auth.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(50) DEFAULT NULL,
@@ -26,11 +26,25 @@ CREATE TABLE IF NOT EXISTS `users` (
   `active_hash` varchar(255) DEFAULT NULL,
   `remember_token` varchar(255) DEFAULT NULL,
   `remember_identifier` varchar(255) DEFAULT NULL,
+  `recover_hash` varchar(255) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+-- Dumping structure for table auth.users_roles
+CREATE TABLE IF NOT EXISTS `users_roles` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `users_roles_users_foreign_key` (`user_id`),
+  CONSTRAINT `users_roles_users_foreign_key` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
