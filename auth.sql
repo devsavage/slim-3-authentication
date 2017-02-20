8d88038a48537a50cdc0c50f92d58797038f5481
Savage <savageboy74@gmail.com>
2017-02-20 00:41:16 -0500

    Update sql file to support new role/permission system.

--- auth.sql
+++ auth.sql
@@ -12,11 +12,31 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Dumping database structure for auth
-CREATE DATABASE IF NOT EXISTS `auth` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `auth`;
+-- Dumping database structure for auth-v2
+CREATE DATABASE IF NOT EXISTS `auth-v2` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `auth-v2`;
 
--- Dumping structure for table auth.roles
+-- Dumping structure for table auth-v2.permissions
+CREATE TABLE IF NOT EXISTS `permissions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table auth-v2.permissions: ~6 rows (approximately)
+/*!40000 ALTER TABLE `permissions` DISABLE KEYS */;
+INSERT INTO `permissions` (`id`, `name`, `created_at`, `updated_at`) VALUES
+	(1, 'delete users', '2017-02-19 23:37:37', '2017-02-19 23:37:56'),
+	(2, 'manage roles', '2017-02-19 23:37:50', '2017-02-20 00:11:25'),
+	(3, 'edit users', '2017-02-19 23:38:42', '2017-02-19 23:38:42'),
+	(4, 'edit admins', '2017-02-19 23:39:39', '2017-02-19 23:39:39'),
+	(5, 'view admin pages', '2017-02-20 00:20:18', '2017-02-20 00:20:18'),
+	(6, 'make admin', '2017-02-20 00:31:23', '2017-02-20 00:31:23');
+/*!40000 ALTER TABLE `permissions` ENABLE KEYS */;
+
+-- Dumping structure for table auth-v2.roles
 CREATE TABLE IF NOT EXISTS `roles` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
@@ -25,8 +45,40 @@ CREATE TABLE IF NOT EXISTS `roles` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
--- Dumping structure for table auth.users
+-- Dumping data for table auth-v2.roles: ~2 rows (approximately)
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` (`id`, `title`, `created_at`, `updated_at`) VALUES
+	(1, 'admin', '2017-02-19 23:37:00', '2017-02-19 23:37:02'),
+	(2, 'superadmin', '2017-02-19 23:37:09', '2017-02-19 23:37:14');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+
+-- Dumping structure for table auth-v2.roles_permissions
+CREATE TABLE IF NOT EXISTS `roles_permissions` (
+  `role_id` int(10) unsigned NOT NULL,
+  `permission_id` int(10) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`role_id`,`permission_id`),
+  KEY `roles_permissions_permission_foreign_key` (`permission_id`),
+  CONSTRAINT `roles_permissions_permission_foreign_key` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `roles_permissions_role_foreign_key` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table auth-v2.roles_permissions: ~9 rows (approximately)
+/*!40000 ALTER TABLE `roles_permissions` DISABLE KEYS */;
+INSERT INTO `roles_permissions` (`role_id`, `permission_id`, `created_at`, `updated_at`) VALUES
+	(1, 1, '2017-02-20 00:34:50', '2017-02-20 00:34:50'),
+	(1, 3, '2017-02-19 23:38:49', '2017-02-19 23:38:49'),
+	(1, 5, '2017-02-20 00:20:36', '2017-02-20 00:20:36'),
+	(2, 1, '2017-02-19 23:38:20', '2017-02-19 23:38:20'),
+	(2, 2, '2017-02-19 23:38:25', '2017-02-19 23:38:25'),
+	(2, 3, '2017-02-20 00:13:12', '2017-02-20 00:13:12'),
+	(2, 4, '2017-02-19 23:39:58', '2017-02-19 23:39:58'),
+	(2, 5, '2017-02-20 00:20:44', '2017-02-20 00:20:44'),
+	(2, 6, '2017-02-20 00:31:32', '2017-02-20 00:31:32');
+/*!40000 ALTER TABLE `roles_permissions` ENABLE KEYS */;
+
+-- Dumping structure for table auth-v2.users
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `username` varchar(50) DEFAULT NULL,
@@ -44,8 +96,27 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
--- Dumping structure for table auth.users_roles
+-- Dumping data for table auth-v2.users: ~0 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+-- Dumping structure for table auth-v2.users_permissions
+CREATE TABLE IF NOT EXISTS `users_permissions` (
+  `user_id` int(10) unsigned NOT NULL,
+  `permission_id` int(10) unsigned NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY `users_permissions_user_foreign_key` (`user_id`),
+  KEY `users_permissions_permission_foreign_key` (`permission_id`),
+  CONSTRAINT `users_permissions_permission_foreign_key` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `users_permissions_user_foreign_key` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Dumping data for table auth-v2.users_permissions: ~0 rows (approximately)
+/*!40000 ALTER TABLE `users_permissions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_permissions` ENABLE KEYS */;
+
+-- Dumping structure for table auth-v2.users_roles
 CREATE TABLE IF NOT EXISTS `users_roles` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int(11) unsigned NOT NULL,
@@ -59,7 +130,10 @@ CREATE TABLE IF NOT EXISTS `users_roles` (
   CONSTRAINT `users_roles_users_foreign_key` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- Data exporting was unselected.
+-- Dumping data for table auth-v2.users_roles: ~0 rows (approximately)
+/*!40000 ALTER TABLE `users_roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users_roles` ENABLE KEYS */;
+
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
